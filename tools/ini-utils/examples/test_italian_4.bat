6e807ba3b566123f6c30b1b5dfb90d859c3f3f08
xGattoMattox <149336969+xGattoMattox@users.noreply.github.com>
2025-01-11 17:32:06 +0100

    Update 4.0.1
    Update 4.0.1

--- tools/ini-utils/examples/test_italian_4.bat
+++ tools/ini-utils/examples/test_italian_4.bat
@@ -1,3 +1,3 @@
 del \validate.txt
-node ../dist/src/index.js validate ../../../data/Localization/english/4.0_global.ini ../../../data/Localization/italian_(italy)/global.ini >> \validate.txt
+node ../dist/src/index.js validate ../../../data/Localization/english/4.0.1_global.ini ../../../data/Localization/italian_(italy)/global.ini >> \validate.txt
 start \validate.txt

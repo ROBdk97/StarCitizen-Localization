ca20fbbd68d8107b7b62417c500231649f04448f
xGattoMattox <149336969+xGattoMattox@users.noreply.github.com>
2025-03-07 19:46:37 +0100

    4.0.2
    4.0.2

--- tools/ini-utils/examples/test_italian_4.0.1.bat
+++
@@ -1,3 +0,0 @@
-del \validate.txt
-node ../dist/src/index.js validate ../../../data/Localization/english/4.0.1_global.ini ../../../data/Localization/italian_(italy)/global.ini >> \validate.txt
-start \validate.txt

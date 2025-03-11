9df1aeccd8a026169bde0ed71e7203d6b447d70f
xGattoMattox <149336969+xGattoMattox@users.noreply.github.com>
2025-03-11 12:18:41 +0100

    4.1 9610516 PTU
    4.1 9610516 PTU

---
+++ tools/ini-utils/examples/test_italian_latest.bat
@@ -0,0 +1,3 @@
+del \validate.txt
+node ../dist/src/index.js validate C:\Users\itali\Desktop\TraduciSC\ULTIMO_global.ini ../../../data/Localization/italian_(italy)/global.ini >> \validate.txt
+start \validate.txt
